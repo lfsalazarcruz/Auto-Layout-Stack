a011cb9f32d4711e033a35556a2d2a5b5c98ad0c
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-08 13:45:42 -0800

    Created calculator layout using stacks and constraints.
    Responsive layout for every device.

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by luis salazar on 12/8/18.
+//  Copyright © 2018 F Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
